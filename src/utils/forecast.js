bb2327614050814a42afaeeb1557264aab4c6e14
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2020-02-11 12:33:30 +0100

    Change language

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'https://api.darksky.net/forecast/9a4b42e7d5b488e904f663535844f76d/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '&lang=it'
+    const url = 'https://api.darksky.net/forecast/9a4b42e7d5b488e904f663535844f76d/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?lang=it'
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
