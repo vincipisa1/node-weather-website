3c18860e42ce20b79f2a5c6347b32f06db971cff
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2020-02-11 12:38:42 +0100

    Change language

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'https://api.darksky.net/forecast/9a4b42e7d5b488e904f663535844f76d/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?lang=it'
+    const url = 'https://api.darksky.net/forecast/9a4b42e7d5b488e904f663535844f76d/' + encodeURIComponent(lat) + ',' + encodeURIComponent(long) + '?lang=it&units=si'
 
     request({url, json: true}, (error, {body}) => {
         if (error) {
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location', undefined)
         } else {
             const currently = body.currently
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + currently.temperature + ' degrees out. There is a ' + currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' Attualmente ci sono ' + currently.temperature + ' fuori. Ci sono ' + currently.precipProbability + '% probabilità di pioggia')
         }
     })
 }
