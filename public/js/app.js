fd757c0a56f4fd05b3ae9da4467ae06bb8203e98
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2020-02-11 10:32:16 +0100

    remove unsued console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-// console.log('Client side javascript file is loaded')
-
 const fetchWeather = (location) => {
     fetch('http://localhost:3000/weather?address='+ location).then((response) => {
         response.json().then((data) => {
